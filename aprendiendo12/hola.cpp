e9874869b659ab99b4b535d2247f735eece0e84f
mucacran <el.mucacran.rasta@hotmail.com>
2017-01-29 09:10:21 -0500

    No se que hago

--- aprendiendo12/hola.cpp
+++ aprendiendo12/hola.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main()
 {
-    cout << "Lendi\n";
+    vector<int> v(4);
+    for(int i=0; i<v.size();i++)v[i]=i+1;
+    
+    vector<int> ve(4);
+    for(int i=0; i<ve.size();i++)ve[i]=i+1;
+
+    vector<int>::iterator b = v.begin();
+    b++;
+    v.insert(b,2);
+
+    while(b!=v.end()) cout << *b << ", ";
 
     return 0;
 }
